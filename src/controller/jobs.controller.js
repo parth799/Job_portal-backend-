1c656f428288a15c7929b4f3cda11a0c3f188749
parth799 <118920131+parth799@users.noreply.github.com>
2024-09-19 11:20:03 +0530

    feat: Add filter-based job fetching and refactor

--- src/controller/jobs.controller.js
+++ src/controller/jobs.controller.js
@@ -55,38 +55,38 @@ const createJob = asyncHandler(async (req, res) => {
 const updateJob = asyncHandler(async (req, res) => {
     const { id } = req.params;
     const userId = req.user._id;
-  
+
     const job = await Job.findById(id);
-  
+
     if (!job) {
-      throw new ApiError(404, "Job not found");
+        throw new ApiError(404, "Job not found");
     }
-  
+
     if (String(job.postedBy) !== String(userId)) {
-      throw new ApiError(403, "You are not authorized to update this job");
+        throw new ApiError(403, "You are not authorized to update this job");
     }
-  
+
     const {
-      title,
-      jobType,
-      location,
-      companyName,
-      introduction,
-      responsibilities,
-      qualifications,
-      offers,
-      salary,
-      hiringMultipleCandidates,
-      personalWebsiteTitle,
-      personalWebsiteUrl,
-      jobNiche
+        title,
+        jobType,
+        location,
+        companyName,
+        introduction,
+        responsibilities,
+        qualifications,
+        offers,
+        salary,
+        hiringMultipleCandidates,
+        personalWebsiteTitle,
+        personalWebsiteUrl,
+        jobNiche
     } = req.body;
-  
+
     if ((personalWebsiteTitle && !personalWebsiteUrl) ||
         (!personalWebsiteTitle && personalWebsiteUrl)) {
-      throw new ApiError(400, "Provide both the website url and title, or leave both blank");
+        throw new ApiError(400, "Provide both the website url and title, or leave both blank");
     }
-  
+
     job.title = title || job.title;
     job.jobType = jobType || job.jobType;
     job.location = location || job.location;
@@ -98,15 +98,36 @@ const updateJob = asyncHandler(async (req, res) => {
     job.salary = salary || job.salary;
     job.hiringMultipleCandidates = hiringMultipleCandidates !== undefined ? hiringMultipleCandidates : job.hiringMultipleCandidates;
     job.personalWebsite = {
-      title: personalWebsiteTitle || job.personalWebsite.title,
-      url: personalWebsiteUrl || job.personalWebsite.url
+        title: personalWebsiteTitle || job.personalWebsite.title,
+        url: personalWebsiteUrl || job.personalWebsite.url
     };
     job.jobNiche = jobNiche || job.jobNiche;
-  
-    await job.save();
-  
-    return res.status(200).json(new ApiResponse(200, job, "Job updated successfully"));
-  });
-  
 
-export {createJob, updateJob}
+    await job.save();
+
+    return res.status(200).json(new ApiResponse(200, job, "Job updated successfully"));
+});
+
+
+const getAllJobs = asyncHandler(async (req, res) => {
+    const { city, niche, searchKeyword } = req.query;
+
+    const query = {};
+
+    if (city) query.location = city;
+
+    if (niche) query.jobNiche = niche;
+
+    if (searchKeyword) {
+        query.$or = [
+            { title: { $regex: searchKeyword, $options: "i" } },
+            { companyName: { $regex: searchKeyword, $options: "i" } },
+            { introduction: { $regex: searchKeyword, $options: "i" } },
+        ];
+    }
+    const jobs = await Job.find(query).sort({ jobPostedOn: -1 });
+    const jobCount = jobs.length;
+    return res.status(200).json(new ApiResponse(200, {jobs, count: jobCount}, "Jobs fetched successfully."));
+})
+
+export { createJob, updateJob, getAllJobs }
--- src/router/job.routes.js
+++ src/router/job.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { isAuthenticated, isAuthorized } from '../middlewares/auth.middlewares.js';
-import { createJob, updateJob } from '../controller/jobs.controller.js';
+import { createJob, updateJob, getAllJobs } from '../controller/jobs.controller.js';
 
 const router = express.Router();
 
 router.post("/create-job", isAuthenticated, isAuthorized("Employer"), createJob);
 router.put("/update-job/:id", isAuthenticated, isAuthorized("Employer"), updateJob);
+router.get("/get-all-jobs", getAllJobs)
+
 
 export default router;
