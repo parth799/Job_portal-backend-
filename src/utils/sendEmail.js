755ec8f4c61f4c24366d838f87be623769005f84
parth799 <118920131+parth799@users.noreply.github.com>
2024-09-20 11:27:06 +0530

    refactor: Streamline email utility code

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,11 +4,9 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// Fix __dirname for ES6
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Updated sendEmail to render EJS template
 const sendEmail = async ({ email, subject, template, templateData }) => {
     const transporter = nodeMailer.createTransport({
         host: process.env.SMTP_HOST,
@@ -20,17 +18,15 @@ const sendEmail = async ({ email, subject, template, templateData }) => {
         },
     });
 
-    // Path to the EJS template
     const templatePath = path.join(__dirname, "../mail", template);
 
-    // Render the EJS template with provided data
     const htmlContent = await ejs.renderFile(templatePath, templateData);
 
     const mailOptions = {
         from: process.env.SMTP_MAIL,
         to: email,
         subject,
-        html: htmlContent, // Set HTML content instead of text
+        html: htmlContent, 
     };
 
     await transporter.sendMail(mailOptions);
